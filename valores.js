045fc0177d557f952534ba751a05b6f68db3d406
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-04-27 20:52:15 +0200

    initial commit

---
+++ valores.js
@@ -0,0 +1,19 @@
+// valores primitivos
+
+40;
+
+("Fernando Gomez");
+
+true;
+false;
+
+null;
+undefined;
+
+// valores no primitivos o tipo objeto
+
+array: [1, 2, 3, 4];
+
+objeto: {
+  nombre: "Fernando";
+}
